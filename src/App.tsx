25d77c8fcc320a3dfe6e1e787ad589e55f29af47
dizinnes <dizinnes228@gmail.com>
2022-11-24 00:29:13 +0600

    feat(app): Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Badge, Stack, Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TextControlsExample from "./services/TextControlsExample";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <TextControlsExample />
+            <Button as="a" variant="primary" className='col-md-4 col-md-offset-4'>
+                Отправить
+            </Button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/services/TextControlsExample.tsx
@@ -0,0 +1,18 @@
+import Form from 'react-bootstrap/Form';
+
+function TextControlsExample() {
+    return (
+        <Form className="col-md-4 col-md-offset-4">
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Ваше имя</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Ваш текст</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+        </Form>
+    );
+}
+
+export default TextControlsExample;
