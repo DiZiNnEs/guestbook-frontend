bdddc077665b13bcb665492f2d5b1e58363027de
dizinnes <dizinnes228@gmail.com>
2022-11-24 17:18:36 +0600

    refactor: Delete unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 import {Badge, Stack, Button} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TextControlsExample from "./services/TextControlsExample";
 
 function App() {
+    const [count, setCount] = useState(0)
     return (
         <div className="App">
+            <h1>Hello {count}</h1>
             <TextControlsExample />
-            <Button as="a" variant="primary" className='col-md-4 col-md-offset-4'>
+            <Button as="a" variant="primary" className='col-md-4 col-md-offset-4' onClick={() => setCount(count + 1)}>
                 Отправить
             </Button>
         </div>
