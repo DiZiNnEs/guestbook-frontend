2606be9131c0ac6ae36b35b79ba32561666e03c6
dizinnes228 <dizinnes228@gmail.com>
2022-11-29 23:06:58 +0600

    feat(process): Adding validation

--- src/components/FormSend.tsx
+++ src/components/FormSend.tsx
@@ -3,15 +3,17 @@ import {Button} from "react-bootstrap";
 import React, {useState} from "react";
 import axios from "axios";
 import {apiEndpoint} from "../cfg";
+import _ from 'lodash'
 
 function FormSend() {
-    const [name, setName] = useState('')
-    const [text, setText] = useState('')
+    const [username, setUsername] = useState('')
+    const [comments, setComments] = useState('')
+    const [formErrors, setFormErrors] = useState({})
 
     async function sendMessages() {
         const response = await axios.post(`${apiEndpoint}/api/v1/comments`, {
-            username: name,
-            comments: text
+            username: username,
+            comments: comments
         }, {
             headers: {
                 'Content-Type': 'application/json',
@@ -20,16 +22,31 @@ function FormSend() {
         })
     }
 
+    async function handleValidation() {
+        let errors: Record<string, any> = {}
+        let isValidForm = true
+
+        if (_.isEmpty(username)) {
+            isValidForm = false
+            errors['username'] = `Username shouldn't be a empty`
+        }
+
+        if (_.isEmpty(comments)) {
+            isValidForm = false
+            errors['comments'] = `Comments`
+        }
+    }
+
     return (
         <Form className="content-center">
-            <h1>{name}</h1>
+            <h1>{username}</h1>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                 <Form.Label>Ваше имя</Form.Label>
-                <Form.Control type="text" value={name} onChange={(event) => setName(event.target.value)}/>
+                <Form.Control type="text" value={username} onChange={(event) => setUsername(event.target.value)}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Ваш текст</Form.Label>
-                <Form.Control as="textarea" rows={5} value={text} onChange={(event) => setText(event.target.value)}/>
+                <Form.Control as="textarea" rows={5} value={comments} onChange={(event) => setComments(event.target.value)}/>
             </Form.Group>
             <Button as="a" variant="primary" className='col-md-4 col-md-offset-4' type='button' onClick={sendMessages}>
                 Отправить
