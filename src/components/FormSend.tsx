2959b4139d6550379e615c183c7f8676ac471739
dizinnes <dizinnes228@gmail.com>
2022-11-30 01:51:39 +0600

    feat: Library for validation form and use it

--- src/components/FormSend.tsx
+++ src/components/FormSend.tsx
@@ -1,16 +1,30 @@
+import _ from 'lodash'
+
 import Form from 'react-bootstrap/Form';
 import {Button} from "react-bootstrap";
 import React, {useState} from "react";
 import axios from "axios";
+import {useForm as validationUseForm, SubmitHandler} from 'react-hook-form';
 import {apiEndpoint} from "../cfg";
-import _ from 'lodash'
+import {FormInputType} from "../interfaces";
+
+type Inputs = {
+    username: string,
+    comments: string,
+};
 
 function FormSend() {
     const [username, setUsername] = useState('')
     const [comments, setComments] = useState('')
-    const [formErrors, setFormErrors] = useState({})
+
+    const [commentsError, setCommentsError] = useState(false)
+    const [usernameError, setUsernameError] = useState(false)
+
+    const {register, handleSubmit, watch, formState: {errors}} = validationUseForm<Inputs>();
+    const onValidationSubmit: SubmitHandler<Inputs> = data => console.log('Submit handler err:', data);
 
     async function sendMessages() {
+        console.log('err', Object.values(errors))
         const response = await axios.post(`${apiEndpoint}/api/v1/comments`, {
             username: username,
             comments: comments
@@ -20,34 +34,34 @@ function FormSend() {
                 Accept: 'application/json',
             }
         })
-    }
 
-    async function handleValidation() {
-        let errors: Record<string, any> = {}
-        let isValidForm = true
-
-        if (_.isEmpty(username)) {
-            isValidForm = false
-            errors['username'] = `Username shouldn't be a empty`
-        }
-
-        if (_.isEmpty(comments)) {
-            isValidForm = false
-            errors['comments'] = `Comments`
-        }
     }
 
     return (
-        <Form className="content-center">
+        <Form className="content-center p-4" onSubmit={handleSubmit(onValidationSubmit)}>
             <h1>{username}</h1>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                 <Form.Label>Ваше имя</Form.Label>
-                <Form.Control type="text" value={username} onChange={(event) => setUsername(event.target.value)}/>
+                <Form.Control {...register('username', {required: true, minLength: 1})} type="text" value={username}
+                              onChange={(event) => {
+                                  setUsername(event.target.value);
+                                  setUsernameError(false)
+                              }}/>
+                {errors.username && <p className='text-danger'>Username is required.</p>}
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Ваш текст</Form.Label>
-                <Form.Control as="textarea" rows={5} value={comments} onChange={(event) => setComments(event.target.value)}/>
+                <Form.Control as="textarea" {...register('comments', {
+                    required: true,
+                    minLength: {value: 1, message: 'Length should be more 4 char'}
+                })} rows={5}
+                              value={comments} onChange={(event) => {
+                    setComments(event.target.value);
+                    setCommentsError(false)
+                }}/>
+                {errors.comments && <p className='text-danger'>Comments are required.</p>}
             </Form.Group>
+            <input type="submit" className='col-md-4 col-md-offset-4' onClick={sendMessages}/>
             <Button as="a" variant="primary" className='col-md-4 col-md-offset-4' type='button' onClick={sendMessages}>
                 Отправить
             </Button>
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,3 +2,5 @@ export interface IComments {
     text: string
     username: string
 }
+
+export type FormInputType = 'username' | 'comments'
