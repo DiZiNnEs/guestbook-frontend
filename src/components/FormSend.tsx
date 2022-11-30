37bbab92df51fd752503705d9dad45393869deec
dizinnes <dizinnes228@gmail.com>
2022-12-01 01:40:53 +0600

    feat: Create custom validation for form

--- src/components/FormSend.tsx
+++ src/components/FormSend.tsx
@@ -1,33 +1,32 @@
-import _ from 'lodash'
-
 import Form from 'react-bootstrap/Form';
 import {Button} from "react-bootstrap";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {useForm as validationUseForm, SubmitHandler} from 'react-hook-form';
 import {apiEndpoint} from "../cfg";
-import {FormInputType} from "../interfaces";
-
-type Inputs = {
-    username: string,
-    comments: string,
-};
 
 function FormSend() {
     const [username, setUsername] = useState('')
-    const [comments, setComments] = useState('')
+    const [comment, setComment] = useState('')
 
-    const [commentsError, setCommentsError] = useState(false)
-    const [usernameError, setUsernameError] = useState(false)
+    const [usernameDirty, setUsernameDirty] = useState(false)
+    const [commentDirty, setCommentDirty] = useState(false)
 
-    const {register, handleSubmit, watch, formState: {errors}} = validationUseForm<Inputs>();
-    const onValidationSubmit: SubmitHandler<Inputs> = data => console.log('Submit handler err:', data);
+    const [usernameError, setUsernameError] = useState('Имя пользователя не может быть пустым')
+    const [commentError, setCommentError] = useState('Комментарий не может быть пустым')
+
+    const [formValid, setFormValid] = useState(false)
+
+    useEffect(() => {
+        if (usernameError || commentError)
+            setFormValid(false)
+        else
+            setFormValid(true)
+    }, [usernameError, commentError])
 
     async function sendMessages() {
-        console.log('err', Object.values(errors))
         const response = await axios.post(`${apiEndpoint}/api/v1/comments`, {
             username: username,
-            comments: comments
+            comments: comment
         }, {
             headers: {
                 'Content-Type': 'application/json',
@@ -35,34 +34,77 @@ function FormSend() {
             }
         })
 
+        if (response.status === 200) {
+            console.log('Данные успешно добавлены')
+            setComment('')
+        } else {
+            console.log('Данные не были добавлены')
+        }
+    }
+
+    function blurHandler(e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        console.log(e)
+        switch (e.target.name) {
+            case 'username':
+                setUsernameDirty(true)
+                break
+            case 'comment':
+                setCommentDirty(true)
+                break
+            default:
+                break
+        }
+    }
+
+    function usernameHandler(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        setUsername(e.target.value)
+        const testUsername = e.target.value.match(/^[A-Za-z0-9_/\s]*$/)
+        const testUsernameHyperLink = e.target.value.match(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/)
+
+        if (!testUsername || testUsernameHyperLink) {
+            setUsernameError('Допустимо: латинские буквы, цифры, знак нижнего подчеркивания')
+        } else if (!e.target.value) {
+            setUsernameError('Имя пользователя не может быть пустым')
+        } else {
+            setUsernameError('')
+        }
+    }
+
+    function commentHandler(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        setComment(e.target.value)
+        const testComment = e.target.value.match(/^[A-Za-z0-9_/\s]*$/)
+
+        if (!testComment) {
+            setCommentError('Допустимо только латинские буквы, цифры, знак нижнего подчеркивания')
+        } else if (!e.target.value) {
+            setCommentError('Комментарий не может быть пустым')
+        } else {
+            setCommentError('')
+        }
     }
 
     return (
-        <Form className="content-center p-4" onSubmit={handleSubmit(onValidationSubmit)}>
+        <Form className="content-center p-4">
             <h1>{username}</h1>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                 <Form.Label>Ваше имя</Form.Label>
-                <Form.Control {...register('username', {required: true, minLength: 1})} type="text" value={username}
+                <Form.Control type="text" name='username' value={username} onBlur={e => blurHandler(e)}
                               onChange={(event) => {
-                                  setUsername(event.target.value);
-                                  setUsernameError(false)
+                                  usernameHandler(event)
                               }}/>
-                {errors.username && <p className='text-danger'>Username is required.</p>}
+                {(usernameDirty && usernameError) && <p className='text-danger'>{usernameError}</p>}
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Ваш текст</Form.Label>
-                <Form.Control as="textarea" {...register('comments', {
-                    required: true,
-                    minLength: {value: 1, message: 'Length should be more 4 char'}
-                })} rows={5}
-                              value={comments} onChange={(event) => {
-                    setComments(event.target.value);
-                    setCommentsError(false)
+                <Form.Label>Ваш комментарий</Form.Label>
+                <Form.Control as="textarea" name='comment' rows={5}
+                              onBlur={e => blurHandler(e)}
+                              value={comment} onChange={(event) => {
+                    commentHandler(event)
                 }}/>
-                {errors.comments && <p className='text-danger'>Comments are required.</p>}
+                {(commentDirty && commentError) && <p className='text-danger'>{commentError}</p>}
             </Form.Group>
-            <input type="submit" className='col-md-4 col-md-offset-4' onClick={sendMessages}/>
-            <Button as="a" variant="primary" className='col-md-4 col-md-offset-4' type='button' onClick={sendMessages}>
+            <Button disabled={!formValid} variant="primary" className='col-md-4 col-md-offset-4' type='button'
+                    onClick={sendMessages}>
                 Отправить
             </Button>
         </Form>
