0514c9a4c3ab9d7a4d47404ef024c67c4da6b8dd
dizinnes228 <dizinnes228@gmail.com>
2022-12-03 23:39:24 +0600

    refactor(project): Delete unused files, move code to separate file

--- src/components/FormSend.tsx
+++ src/components/FormSend.tsx
@@ -1,115 +1,25 @@
 import Form from 'react-bootstrap/Form';
 import {Button} from "react-bootstrap";
-import React, {useEffect, useState} from "react";
-import axios from "axios";
-import {apiEndpoint} from "../cfg";
+import React from "react";
 import Snackbar from "./tools/Snackbar";
+import useForm from "./hooks/useForm";
 
 function FormSend(props: any) {
-    const [username, setUsername] = useState('')
-    const [comment, setComment] = useState('')
+    const {
+        username,
+        comment,
+        usernameDirty,
+        commentDirty,
+        usernameError,
+        commentError,
+        formValid,
+        isSnackbarShown,
+        blurHandler,
+        usernameHandler,
+        commentHandler,
+        sendMessages
+    } = useForm(props)
 
-    const [usernameDirty, setUsernameDirty] = useState(false)
-    const [commentDirty, setCommentDirty] = useState(false)
-
-    const [usernameError, setUsernameError] = useState('Имя пользователя не может быть пустым')
-    const [commentError, setCommentError] = useState('Комментарий не может быть пустым')
-
-    const [formValid, setFormValid] = useState(false)
-
-    useEffect(() => {
-        console.log({usernameError})
-        console.log({commentError})
-        if (usernameError || commentError) {
-            setFormValid(false)
-            console.log('поставлися', false)
-        } else {
-            setFormValid(true)
-            console.log('поставлися', true)
-        }
-
-        console.log(formValid, 'validForm')
-    }, [usernameError, commentError, formValid])
-
-    async function sendMessages() {
-        setIsSnackbarShown(true)
-        if (formValid) {
-            const response = await axios.post(`${apiEndpoint}/api/v1/comments`, {
-                username: username,
-                comments: comment
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: 'application/json',
-                }
-            })
-
-            if (response.status === 200) {
-                console.log('Данные успешно добавлены')
-                setComment('')
-                props.parentCallback(response.data);
-
-            } else {
-                console.log('Данные не были добавлены')
-            }
-        }
-
-        setIsSnackbarShown(false)
-    }
-
-    function blurHandler(e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) {
-        console.log(e)
-        switch (e.target.name) {
-            case 'username':
-                setUsernameDirty(true)
-                break
-            case 'comment':
-                setCommentDirty(true)
-                break
-            default:
-                break
-        }
-    }
-
-    function usernameHandler(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-        setUsername(e.target.value)
-        const testUsername = e.target.value.match(/^[A-Za-z0-9_/\s]*$/)
-        const testUsernameHyperLink = e.target.value.match(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/)
-
-        if (!testUsername || testUsernameHyperLink) {
-            setUsernameError('Допустимо: латинские буквы, цифры, знак нижнего подчеркивания')
-        } else if (!e.target.value) {
-            setUsernameError('Имя пользователя не может быть пустым')
-        } else {
-            setUsernameError('')
-        }
-    }
-
-    function commentHandler(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-        setComment(e.target.value)
-        const testComment = e.target.value.match(/^[A-Za-z0-9_/\s]*$/)
-
-        if (!testComment) {
-            setCommentError('Допустимо только латинские буквы, цифры, знак нижнего подчеркивания')
-        } else if (!e.target.value) {
-            setCommentError('Комментарий не может быть пустым')
-        } else {
-            setCommentError('')
-        }
-    }
-
-    const keydownHandler = async (e: any) => {
-        if (e.key === 'Enter' && e.ctrlKey) await sendMessages()
-    };
-
-    React.useEffect(() => {
-        document.addEventListener('keydown', keydownHandler);
-        return () => {
-            document.removeEventListener('keydown', keydownHandler);
-        }
-    }, [keydownHandler]);
-
-    const [isSnackbarShown, setIsSnackbarShown] = useState(false)
 
     return (
         <Form className="content-center p-4">
